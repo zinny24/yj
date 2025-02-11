96624116bd38447da6bb5f5d70355eda1e362e22
zinny24 <61670559+zinny24@users.noreply.github.com>
2025-02-11 11:29:02 +0900

    Delete yangjung directory

--- yangjung/src/main/java/com/example/yangjung/articles/ArticlesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.yangjung.articles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticlesRepository extends JpaRepository<Article, Long> {
-
-}
